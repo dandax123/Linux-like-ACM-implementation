875798c5479d8cb398eee304ffa9cd1368c5c28f
DanielOluks <dandaoluks@gmail.com>
2020-12-21 20:27:24 +0200

    feat: get index and id from a name

--- acm.cpp
+++ acm.cpp
@@ -32,6 +32,7 @@ struct groupPermission {
 
 
 struct object {
+	string objectId;
 	string objectOwner;
 	string objectName;
 	struct userPermission currentUserPermissions[20];
@@ -62,6 +63,8 @@ struct system createNewSystem(){
 	mySystem.myObjects.currentObjectIndex = -1;
 	return mySystem;
 }
+struct system mySystem = createNewSystem();
+struct system *mySystemPointer = &mySystem;
 string gen_id() {
     const int len = 30;
     string tmp_s;
@@ -103,24 +106,103 @@ void addGroupToSystem(struct system *mySystem){
 	cin>>mySystem->myGroups.groups[newIndex].groupName;
 	mySystem->myGroups.groups[newIndex].isEnabled = 1;
 	mySystem->myGroups.groups[newIndex].groupId = gen_id();
+	cout<<"Succesfully added!"<<endl;
 }
+
+string getIdFromSystemName(struct system *mySystem, string type, string id){
+	string returnId;
+	if(type== "group"){
+		int currIndex = mySystem->myGroups.currentGroupIndex;
+		for(int i  = 0; i< currIndex + 1; i++){
+			if(mySystem->myGroups.groups[i].groupName == id){
+				returnId=  mySystem->myGroups.groups[i].groupId;
+			}
+		}
+	}
+	else if(type == "users"){
+		int currIndex = mySystem->myUsers.currentUserIndex;
+		for(int i  = 0; i< currIndex + 1; i++){
+			if(mySystem->myUsers.users[i].userName == id){
+				returnId= mySystem->myUsers.users[i].userId; 
+			}
+		}
+	}
+	else {
+		int currIndex = mySystem->myObjects.currentObjectIndex;
+		for(int i  = 0; i< currIndex + 1; i++){
+			if(mySystem->myObjects.objects[i].objectName == id){
+				returnId = mySystem->myObjects.objects[i].objectId;
+			}
+		}
+	}
+	return returnId;
+}
+
+int getIndexFromSystem(struct system *mySystem, string type, string id){
+	int index = 0;
+	if(type== "group"){
+		int currIndex = mySystem->myGroups.currentGroupIndex;
+		for(int i  = 0; i< currIndex + 1; i++){
+			if(mySystem->myGroups.groups[i].groupId == id){
+				index = i;
+			}
+		}
+	}
+	else if(type == "users"){
+		int currIndex = mySystem->myUsers.currentUserIndex;
+		for(int i  = 0; i< currIndex + 1; i++){
+			if(mySystem->myUsers.users[i].userId == id){
+				index = i;
+			}
+		}
+	}
+	else {
+		int currIndex = mySystem->myObjects.currentObjectIndex;
+		for(int i  = 0; i< currIndex + 1; i++){
+			if(mySystem->myObjects.objects[i].objectId == id){
+				index = i;
+			}
+		}
+	}
+	return index;
+}
+void enableOrDisableGroupOrUser(struct system *mySystem, string type, int operation, int index){
+	if(type == "group"){
+		mySystem->myGroups.groups[index].isEnabled = operation;	
+	}
+	else{
+		mySystem->myUsers.users[index].isEnabled = operation;	
+	}
+	string result = operation == 1 ? "Enabled" : "Disabled";
+	cout<< "Successfully "<<result<<endl;
+}
+
+
 
 void displayAllUsers (struct system *mySystem) {
 	int currIndex = mySystem->myUsers.currentUserIndex;
 	int i = 0;
 	for(i=0; i< currIndex+1; i++){
 		cout<<"the user name is  "<<mySystem->myUsers.users[i].userName;
+		
 	}
 }
 
+void enableOperationForUser (struct system *mySystem){
+	string userName;
+	cout<<"Enter the user Name of the user"<<endl;
+	cin>>userName;
+	int index = getIndexFromSystem(mySystemPointer, "users", getIdFromSystemName(mySystemPointer,"users", userName)); 
+	cout<<index;
+}
 
 
 int main (){
-	struct system mySystem = createNewSystem();
-	struct system *mySystemPointer = &mySystem;
+
 	addUserToSystem(mySystemPointer);
 	addGroupToSystem(mySystemPointer);
 	displayAllUsers(mySystemPointer);
+	enableOperationForUser(mySystemPointer);
 	return 0;
 }
 
